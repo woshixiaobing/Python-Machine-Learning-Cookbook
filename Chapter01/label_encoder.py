7c0cc7e4885bcc243868b439f67fae64463a2c4a
Li Chao <leetschau@gmail.com>
2017-08-30 09:31:05 +0800

    use spyder-style cell delimiter; label encoding verified

--- Chapter01/label_encoder.py
+++ Chapter01/label_encoder.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 from sklearn import preprocessing
 
@@ -5,19 +6,20 @@ label_encoder = preprocessing.LabelEncoder()
 input_classes = ['audi', 'ford', 'audi', 'toyota', 'ford', 'bmw']
 label_encoder.fit(input_classes)
 
-# print classes
-print "\nClass mapping:"
+#%% print classes
+# NOTE: the labels are sorted by alphabetical order, not order in the list
+print("\nClass mapping:")
 for i, item in enumerate(label_encoder.classes_):
-    print item, '-->', i
+    print(item, '-->', i)
 
-# transform a set of classes
+#%% transform a set of classes
 labels = ['toyota', 'ford', 'audi']
 encoded_labels = label_encoder.transform(labels)
-print "\nLabels =", labels 
-print "Encoded labels =", list(encoded_labels)
+print("\nLabels =", labels)
+print("Encoded labels =", list(encoded_labels))
 
-# inverse transform
+#%% inverse transform
 encoded_labels = [2, 1, 0, 3, 1]
 decoded_labels = label_encoder.inverse_transform(encoded_labels)
-print "\nEncoded labels =", encoded_labels
-print "Decoded labels =", list(decoded_labels)
+print("\nEncoded labels =", encoded_labels)
+print("Decoded labels =", list(decoded_labels))
--- Chapter01/preprocessing.py
+++ Chapter01/preprocessing.py
@@ -1,3 +1,4 @@
+#%% data
 import numpy as np
 from sklearn import preprocessing
 
@@ -5,17 +6,17 @@ data = np.array([[ 3, -1.5,  2, -5.4],
                  [ 0,  4,  -0.3, 2.1],
                  [ 1,  3.3, -1.9, -4.3]])
 
-# mean removal
+#%% mean removal
 data_standardized = preprocessing.scale(data)
 print("\nMean =", data_standardized.mean(axis=0))
 print("Std deviation =", data_standardized.std(axis=0))
 
-# min max scaling
+#%% min max scaling
 data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 data_scaled = data_scaler.fit_transform(data)
 print("\nMin max scaled data:\n", data_scaled)
 
-# normalization
+#%% normalization
 data_normalized = preprocessing.normalize(data, norm='l1')
 print("L1 normalized data:\n", data_normalized)
 print('L1 norm of every sample for a L1 normed matrics:')
@@ -34,12 +35,14 @@ print('L2 norm of every sample for a L2 normed matrics:')
 for i in range(len(data_norm_l2)):
     print(np.linalg.norm(data_norm_l2[i], ord=2))
     
-# binarization
+#%% binarization
 data_binarized = preprocessing.Binarizer(threshold=1.4).transform(data)
 print("\nBinarized data:\n", data_binarized)
 
-# one hot encoding
+#%% one hot encoding
 encoder = preprocessing.OneHotEncoder()
 encoder.fit([[0, 2, 1, 12], [1, 3, 5, 3], [2, 3, 2, 12], [1, 2, 4, 3]])
 encoded_vector = encoder.transform([[2, 3, 5, 3]]).toarray()
 print("\nEncoded vector:\n", encoded_vector)
+
+##
